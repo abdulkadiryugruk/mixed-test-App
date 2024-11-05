383e240fad002b5ac1dc62f91d9dc280055842cc
Abdülkadir <abdulkadiryugruk@gmail.com>
2024-11-06 00:14:33 +0300

    1.0.2

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './src/screens/HomeScreen';
-import CoursesScreen from './src/screens/CoursesScreen';
-import CoursesInformationScreen from './src/screens/CoursesInformationScreen';
-import CounterScreen from './src/screens/CounterScreen';
-import BoxScreen from './src/screens/BoxScreen';
-import ColorChangeScreen from './src/screens/ColorChangeScreen';
-import PasswordScreen from './src/screens/PasswordScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./src/screens/HomeScreen";
+import CoursesScreen from "./src/screens/CoursesScreen";
+import CoursesInformationScreen from "./src/screens/CoursesInformationScreen";
+import CounterScreen from "./src/screens/CounterScreen";
+import BoxScreen from "./src/screens/BoxScreen";
+import ColorChangeScreen from "./src/screens/ColorChangeScreen";
+import PasswordScreen from "./src/screens/PasswordScreen";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -14,17 +14,16 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Anasayfa" component={HomeScreen} />
-        <Stack.Screen name="Kurslarım" component={CoursesScreen} />
+        <Stack.Screen name="Kurslarim" component={CoursesScreen} />
         <Stack.Screen
           name="KursBilgilerim"
           component={CoursesInformationScreen}
         />
         <Stack.Screen name="Sayac" component={CounterScreen} />
-        <Stack.Screen name="Kutu Uygulaması" component={BoxScreen} />
+        <Stack.Screen name="Kutu Uygulamasi" component={BoxScreen} />
         <Stack.Screen name="Renk degistir" component={ColorChangeScreen} />
         <Stack.Screen name="Password" component={PasswordScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
